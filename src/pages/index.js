018409a2af2ce1c490d17b94c301a0e96b529ec1
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-12-20 23:52:20 +0000

    adding meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,29 @@ export default function Home() {
           content="Fast, reliable & simple conference calls from all around the world."
         />
         <link rel="icon" href="/favicon.svg" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://444comms.vercel.app/" />
+        <meta
+          property="og:title"
+          content="444Coms - Simple Conference Calls."
+        />
+        <meta
+          property="og:description"
+          content="Fast, reliable & simple conference calls from all around the world."
+        />
+        <meta property="og:image" content="" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://444comms.vercel.app/" />
+        <meta
+          property="twitter:title"
+          content="444Coms - Simple Conference Calls."
+        />
+        <meta
+          property="twitter:description"
+          content="Fast, reliable & simple conference calls from all around the world."
+        />
+        <meta property="twitter:image" content="" />
       </Head>
 
       <Hero />
